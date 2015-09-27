cbea302ce41cabe284b7c86849de0e82efc7d589
Kikyou Akino <bellflower@web4u.jp>
2015-09-27 10:14:35 +0900

    Added '--noask' option

--- setup.bash
+++ setup.bash
@@ -3,5 +3,5 @@
 url='https://raw.githubusercontent.com/bellflower2015/pspkg/master/bin/pspkg'
 tmpfile=$(mktemp -t pspkg)
 curl -L $url -o $tmpfile
-bash $tmpfile
+bash $tmpfile --noask
 unlink $tmpfile
